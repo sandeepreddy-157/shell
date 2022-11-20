e359d443c1ea4434ef2146587d64740a0878196b
sandeepabbadi <sandeepabbadi.vts@gmail.com>
2022-11-20 21:03:39 +0530

    shell topics

---
+++ shell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Initializing two variables
+a=20
+b=20
+
+if [ $a == $b ]
+then
+    #If they are equal then print this
+    echo "a is equal to b"
+else
+    #else print this
+    echo "a is not equal to b"
+fi
