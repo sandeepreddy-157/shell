a97da8ee11de0c846d829a7775e3aa10d3479fc6
sandeepabbadi <71769584+sandeepreddy-157@users.noreply.github.com>
2022-12-07 16:19:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAQU3C4FVIZPCYJRGO"           #access key id
-  secret_key = "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" #secret key id
+  access_key = "ACCESS_KEY"           #access key id
+  secret_key = "SECRET_KEY"           #secret key id
 }
 
 resource "aws_instance" "ubuntu" {
